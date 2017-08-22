fea3c153b133686bed9dcacfcc45c889b0437c9d
Kidhar Bachan <Kidhar.Bachan@absa.co.za>
2017-08-22 07:31:40 +0200

    Fixed chef-19 deprecation issues in fixpack recipe

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'Kidhar.Bachan@absa.co.za'
 license 'Apache 2.0'
 description 'Installs/Configures IBM Installation Manager fork of https://github.com/Sainsburys/ibm-installmgr'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.2.4'
+version '1.2.5'
