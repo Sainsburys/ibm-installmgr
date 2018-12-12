adf84aa0a875ec7eda98a9912bba3666e5832feb
Chris Minton <christopher.minton@sainsburys.co.uk>
2018-12-12 14:59:52 +0000

    fix foodcritic errors in metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,13 @@
 name 'ibm-installmgr'
 maintainer 'Sainsburys Devops'
 maintainer_email 'devops@sainsburys.co.uk'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs/Configures IBM Installation Manager'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.3.0'
+supports 'redhat'
+supports 'centos'
+
+issues_url 'https://github.com/Sainsburys/ibm-installmgr/issues'
+source_url 'https://github.com/Sainsburys/ibm-installmgr'
+chef_version '>= 12.5' if respond_to?(:chef_version)
